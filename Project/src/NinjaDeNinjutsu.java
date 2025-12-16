608e571681cc307b836f33b51467065b520abbe5
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-12-16 18:24:38 -0300

    feat: implementa NinjaDeNinjutsu com uso de jutsu, consumo de chakra e chance de desvio

---
+++ Project/src/NinjaDeNinjutsu.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public abstract class NinjaDeNinjutsu extends Personagem implements Ninja {
+    public NinjaDeNinjutsu(String nome, int vida) {
+        super(nome, vida);
+    }
+
+    @Override
+    public void usarJutsu(Ninja inimigo) {
+        Jutsu jutsu = jutsus.values().iterator().next();
+
+        if (!temChakra(jutsu.getConsumoDeChakra())) {
+            System.out.println(nome + " Está sem chakra para usar Ninjutsu!");
+            return;
+        }
+
+        gastarChakra(jutsu.getConsumoDeChakra());
+        System.out.println(nome + " Usou Ninjutsu causando " + jutsu.getDano() + " de dano!");
+        inimigo.desviar(jutsu.getDano());
+    }
+
+    @Override
+    public void desviar(int dano) {
+        boolean conseguiuDesviar = new Random().nextInt(100) < 40;
+
+        if (conseguiuDesviar) {
+            System.out.println(nome + " Desviou do Ninjutsu!");
+        } else {
+            receberDano(dano);
+            System.out.println(nome + " Foi atingido e perdeu " + dano + " de vida!");
+        }
+    }
+}
